2e01826835fb3a744b9520624341752b6ba9445d
nothing-c <halskdjf@gmail.com>
2025-03-12 22:34:30 -0500

    File of matrix locations & solutions; only a handful rn, but this is the data format I want

---
+++ matrices.fs
@@ -0,0 +1,192 @@
+[("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-01\Basic Problem B-01.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-02\Basic Problem B-02.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-03\Basic Problem B-03.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-04\Basic Problem B-04.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-05\Basic Problem B-05.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-06\Basic Problem B-06.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-07\Basic Problem B-07.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-08\Basic Problem B-08.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-09\Basic Problem B-09.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-10\Basic Problem B-10.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-11\Basic Problem B-11.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems B\Basic Problem B-12\Basic Problem B-12.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-01\Basic Problem C-01.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-02\Basic Problem C-02.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-03\Basic Problem C-03.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-04\Basic Problem C-04.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-05\Basic Problem C-05.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-06\Basic Problem C-06.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-07\Basic Problem C-07.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-08\Basic Problem C-08.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-09\Basic Problem C-09.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-10\Basic Problem C-10.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-11\Basic Problem C-11.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems C\Basic Problem C-12\Basic Problem C-12.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-01\Basic Problem D-01.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-02\Basic Problem D-02.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-03\Basic Problem D-03.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-04\Basic Problem D-04.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-05\Basic Problem D-05.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-06\Basic Problem D-06.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-07\Basic Problem D-07.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-08\Basic Problem D-08.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-09\Basic Problem D-09.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-10\Basic Problem D-10.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-11\Basic Problem D-11.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems D\Basic Problem D-12\Basic Problem D-12.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-01\Basic Problem E-01.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-02\Basic Problem E-02.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-03\Basic Problem E-03.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-04\Basic Problem E-04.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-05\Basic Problem E-05.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-06\Basic Problem E-06.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-07\Basic Problem E-07.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-08\Basic Problem E-08.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-09\Basic Problem E-09.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-10\Basic Problem E-10.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-11\Basic Problem E-11.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Basic Problems E\Basic Problem E-12\Basic Problem E-12.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-01\Challenge Problem B-01.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-02\Challenge Problem B-02.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-03\Challenge Problem B-03.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-04\Challenge Problem B-04.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-05\Challenge Problem B-05.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-06\Challenge Problem B-06.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-07\Challenge Problem B-07.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-08\Challenge Problem B-08.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-09\Challenge Problem B-09.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-10\Challenge Problem B-10.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-11\Challenge Problem B-11.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems B\Challenge Problem B-12\Challenge Problem B-12.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-01\Challenge Problem C-01.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-02\Challenge Problem C-02.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-03\Challenge Problem C-03.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-04\Challenge Problem C-04.PNG",
+  "8");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-05\Challenge Problem C-05.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-06\Challenge Problem C-06.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-07\Challenge Problem C-07.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-08\Challenge Problem C-08.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-09\Challenge Problem C-09.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-10\Challenge Problem C-10.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-11\Challenge Problem C-11.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems C\Challenge Problem C-12\Challenge Problem C-12.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-01\Challenge Problem D-01.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-02\Challenge Problem D-02.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-03\Challenge Problem D-03.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-04\Challenge Problem D-04.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-05\Challenge Problem D-05.PNG",
+  "2");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-06\Challenge Problem D-06.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-07\Challenge Problem D-07.PNG",
+  "4");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-08\Challenge Problem D-08.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-09\Challenge Problem D-09.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-10\Challenge Problem D-10.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-11\Challenge Problem D-11.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems D\Challenge Problem D-12\Challenge Problem D-12.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-01\Challenge Problem E-01.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-02\Challenge Problem E-02.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-03\Challenge Problem E-03.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-04\Challenge Problem E-04.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-05\Challenge Problem E-05.PNG",
+  "6");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-06\Challenge Problem E-06.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-07\Challenge Problem E-07.PNG",
+  "5");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-08\Challenge Problem E-08.PNG",
+  "7");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-09\Challenge Problem E-09.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-10\Challenge Problem E-10.PNG",
+  "1");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-11\Challenge Problem E-11.PNG",
+  "3");
+ ("./Ravens-Progressive-Matrices/Problems\Challenge Problems E\Challenge Problem E-12\Challenge Problem E-12.PNG",
+  "5")]
