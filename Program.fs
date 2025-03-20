69bc842a1687aad49e6f1a332a72bc7acfa4139f
nothing-c <halskdjf@gmail.com>
2025-03-19 21:06:56 -0500

    Finally getting the hang of request handling

--- Program.fs
+++ Program.fs
@@ -1,13 +1,21 @@
 open System
+open System.IO
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
+open System.Text
+
+let mainpage = File.ReadAllText("page.html")
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
+    // printfn "%A" builder.Environment
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/", Func<_>(fun () -> Results.Text(mainpage,"text/html",Encoding.UTF8) )) |> ignore
+    app.MapGet("/answer", Func<_>(fun x -> printfn "%A" x)) |> ignore
+    // /answer?answer=...
 
     app.Run()
 
