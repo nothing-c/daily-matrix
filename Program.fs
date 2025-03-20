506c148cab6ee93e419a8d21d4cc5e14969ec7d2
nothing-c <halskdjf@gmail.com>
2025-03-19 23:01:03 -0500

    Get the answering actually fucking working. Yay! Now it's configuring the server to pick the matrix & answer every 0000, then CSS & deployment!

--- Program.fs
+++ Program.fs
@@ -1,21 +1,31 @@
 open System
 open System.IO
+open System.Web
+open System.Text
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.AspNetCore.Http
-open System.Text
+//open Microsoft.AspNet.StaticFiles
 
 let mainpage = File.ReadAllText("page.html")
+
+let answer (context: HttpContext) (correct: int) =
+    let a = context.Request.Query in
+    let actualanswer = (Seq.head a).Value |> Seq.head |> int in
+    if actualanswer = correct then "Yay" else "Boo"
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
-    // printfn "%A" builder.Environment
     let app = builder.Build()
+    app.UseStaticFiles() |> ignore
 
-    app.MapGet("/", Func<_>(fun () -> Results.Text(mainpage,"text/html",Encoding.UTF8) )) |> ignore
-    app.MapGet("/answer", Func<_>(fun x -> printfn "%A" x)) |> ignore
+    let correct = 0
+    // app.MapGet("/", Func<_>(fun () -> Results.Text(mainpage,"text/html",Encoding.UTF8) )) |> ignore
     // /answer?answer=...
+
+    app.MapGet("/answer", Func<HttpContext, string>(fun x -> answer x correct)) |> ignore
+    // so Func<arg, arg..., returntype>
 
     app.Run()
 
