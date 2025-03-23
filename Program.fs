0e4975ad43678045adfc9f8474247e4ac6ae71b1
nothing-c <halskdjf@gmail.com>
2025-03-23 14:09:04 -0500

    Add loop to change matrix every day

--- Program.fs
+++ Program.fs
@@ -7,6 +7,7 @@ open Microsoft.Extensions.Hosting
 open Microsoft.AspNetCore.Http
 
 let mainpage = File.ReadAllText("page.html")
+let rand = new Random()
 
 let answer (context: HttpContext) (correct: string) =
     let a = context.Request.Query in
@@ -15,17 +16,26 @@ let answer (context: HttpContext) (correct: string) =
 
 [<EntryPoint>]
 let main args =
-    let rand = new Random()
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
     app.UseStaticFiles() |> ignore
 
-    let (matrixpath, correct) = Matrices.rawmatrices[rand.Next(Matrices.rawmatrices.Length)]
+    let mutable (matrixpath, correct) = Matrices.rawmatrices[rand.Next(Matrices.rawmatrices.Length)] //Grab the first one
     // All the images are PNGs
     app.MapGet("/matrix", Func<_>(fun () -> Results.Bytes(File.ReadAllBytes("wwwroot/" + matrixpath), "image/png"))) |> ignore
     app.MapGet("/answer", Func<HttpContext, string>(fun x -> answer x correct)) |> ignore
 
     app.Run()
+
+    let mutable day = DateTime.UtcNow.DayOfWeek
+    while true do
+        if day <> DateTime.UtcNow.DayOfWeek
+        then
+            day <- DateTime.UtcNow.DayOfWeek
+            // full-sending this, so it may do two of the same in a row. Who cares?
+            let (matrixpath', correct') = Matrices.rawmatrices[rand.Next(Matrices.rawmatrices.Length)]
+            matrixpath <- matrixpath'
+            correct <- correct'
 
     0 // Exit code
 
